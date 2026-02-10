139fc8b42fb96b29501d5083220471ae6681fe01
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-02-10 11:36:40 -0600

    feat: Se ha cargado el mapeo de la de la entidad Proveedores

--- Ejemplo/Ejemplo/src/main/java/com/jerrycastro/RepuestosAutomotriz/entity/Proveedores.java
+++ Ejemplo/Ejemplo/src/main/java/com/jerrycastro/RepuestosAutomotriz/entity/Proveedores.java
@@ -1,5 +1,65 @@
 package com.jerrycastro.RepuestosAutomotriz.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Proveedores")
 public class Proveedores {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer id_proveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombre_proveedor;
+
+    @Column(name = "telefono_proveedor")
+    private Integer telfono_proveedor;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "email_proveedor")
+    private String email_proveedor;
+
+    public Integer getId_proveedor() {
+        return id_proveedor;
+    }
+
+    public void setId_proveedor(Integer id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
+
+    public String getNombre_proveedor() {
+        return nombre_proveedor;
+    }
+
+    public void setNombre_proveedor(String nombre_proveedor) {
+        this.nombre_proveedor = nombre_proveedor;
+    }
+
+    public Integer getTelfono_proveedor() {
+        return telfono_proveedor;
+    }
+
+    public void setTelfono_proveedor(Integer telfono_proveedor) {
+        this.telfono_proveedor = telfono_proveedor;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail_proveedor() {
+        return email_proveedor;
+    }
+
+    public void setEmail_proveedor(String email_proveedor) {
+        this.email_proveedor = email_proveedor;
+    }
 }
