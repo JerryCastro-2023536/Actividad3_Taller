9c413e9fc7c7a44557f038cb5e25330a8507f994
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-02-13 22:14:25 -0600

    feat: Se cargaron las personalizadas del proyecto

--- Ejemplo/Ejemplo/src/main/java/com/jerrycastro/RepuestosAutomotriz/exception/InvalidNumberException.java
+++ Ejemplo/Ejemplo/src/main/java/com/jerrycastro/RepuestosAutomotriz/exception/InvalidNumberException.java
@@ -1,7 +1,7 @@
 package com.jerrycastro.RepuestosAutomotriz.exception;
 
 public class InvalidNumberException extends RuntimeException {
-  public InvalidNumberException(String message) {
-    super(message);
-  }
+    public InvalidNumberException(String message) {
+        super(message);
+    }
 }
